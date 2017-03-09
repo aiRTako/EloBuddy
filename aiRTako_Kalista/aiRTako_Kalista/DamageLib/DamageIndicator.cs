da055271d46942c446b737b606b650de00ed6c8e
aiRTako <a751609885@qq.com>
2017-03-09 15:49:47 +0800

    Kalista

---
+++ aiRTako_Kalista/aiRTako_Kalista/DamageLib/DamageIndicator.cs
@@ -0,0 +1,118 @@
+﻿namespace aiRTako_Kalista.DamageLib
+{
+    using System;
+    using System.Linq;
+    using System.Globalization;
+
+    using SharpDX;
+
+    using EloBuddy;
+    using EloBuddy.SDK;
+    using EloBuddy.SDK.Menu;
+    using EloBuddy.SDK.Menu.Values;
+
+    using Color = System.Drawing.Color;
+    using Line = EloBuddy.SDK.Rendering.Line;
+
+    internal static class DamageIndicator
+    {
+        private static readonly Vector2 BarOffset = new Vector2(1.25f, 14.25f);
+        private static readonly Vector2 PercentOffset = new Vector2(-31, 3);
+        private static readonly Vector2 PercentOffset1 = new Vector2(-30, 3);
+        private static readonly Vector2 PercentOffset2 = new Vector2(-29, 3);
+
+        private static Line _line;
+
+        internal static bool Fill = true;
+        internal static bool Enabled = true;
+
+        internal static Color Color = Color.FromArgb(255, 255, 169, 4);
+
+        private static DamageToUnitDelegate _damageToUnit;
+
+        internal delegate float DamageToUnitDelegate(AIHeroClient hero);
+
+        internal static void AddToMenu(Menu mainMenu, DamageToUnitDelegate Damage = null)
+        {
+            _line = new Line
+            {
+                Antialias = true,
+                Width = 9
+            };
+
+            mainMenu.AddSeparator();
+            mainMenu.AddLabel("Damage Indicator");
+            mainMenu.Add("DrawComboDamage", new CheckBox("Draw E Damage"));
+            mainMenu.Add("DrawFillDamage", new CheckBox("Draw E Damage Fill Color"));
+
+            DamageToUnit = Damage ?? DamageCalculate.GetRealEDamage;
+
+            Enabled = mainMenu["DrawComboDamage"].Cast<CheckBox>().CurrentValue;
+            Fill = mainMenu["DrawFillDamage"].Cast<CheckBox>().CurrentValue;
+
+            Game.OnTick += delegate
+            {
+                Enabled = mainMenu["DrawComboDamage"].Cast<CheckBox>().CurrentValue;
+                Fill = mainMenu["DrawFillDamage"].Cast<CheckBox>().CurrentValue;
+            };
+        }
+
+        internal static DamageToUnitDelegate DamageToUnit
+        {
+            set
+            {
+                if (_damageToUnit == null)
+                    Drawing.OnEndScene += Drawing_OnEndScene;
+
+                _damageToUnit = value;
+            }
+        }
+
+        private static void Drawing_OnEndScene(EventArgs eventArgs)
+        {
+            if (!Enabled || _damageToUnit == null)
+                return;
+
+            foreach (var unit in EntityManager.Heroes.Enemies.Where(u => u.IsValidTarget() && u.IsHPBarRendered))
+            {
+                var damage = _damageToUnit(unit);
+
+                if (damage <= 0)
+                    continue;
+
+                if (unit.Health > 0)
+                {
+                    var text = ((int)(unit.Health - damage)).ToString(CultureInfo.InvariantCulture);
+
+                    if (text.Length == 4)
+                    {
+                        Drawing.DrawText(unit.HPBarPosition + PercentOffset, Color.Red, text, 10);
+                    }
+                    else if (text.Length == 3)
+                    {
+                        Drawing.DrawText(unit.HPBarPosition + PercentOffset1, Color.Red, text, 10);
+                    }
+                    else if (text.Length == 2)
+                    {
+                        Drawing.DrawText(unit.HPBarPosition + PercentOffset2, Color.Red, text, 10);
+                    }
+                    else
+                    {
+                        Drawing.DrawText(unit.HPBarPosition + PercentOffset, Color.Red, text, 10);
+                    }
+                }
+
+                if (Fill)
+                {
+                    var damagePercentage = (unit.TotalShieldHealth() - damage > 0 ? unit.TotalShieldHealth() - damage : 0) / (unit.MaxHealth + unit.AllShield + unit.AttackShield + unit.MagicShield);
+                    var currentHealthPercentage = unit.TotalShieldHealth() / (unit.MaxHealth + unit.AllShield + unit.AttackShield + unit.MagicShield);
+                    var startPoint = new Vector2(unit.HPBarPosition.X + BarOffset.X + damagePercentage * 104, unit.HPBarPosition.Y + BarOffset.Y - 5);
+                    var endPoint = new Vector2(unit.HPBarPosition.X + BarOffset.X + currentHealthPercentage * 104 + 1, unit.HPBarPosition.Y + BarOffset.Y - 5);
+
+                    _line.Draw(Color, startPoint, endPoint);
+                    Drawing.DrawLine(startPoint, endPoint, 9, Color);
+                }
+            }
+        }
+    }
+}
---
+++ aiRTako_Kalista/aiRTako_Kalista/ManaManager.cs
@@ -0,0 +1,70 @@
+﻿namespace aiRTako_Kalista
+{
+    using EloBuddy;
+    using EloBuddy.SDK.Menu;
+    using EloBuddy.SDK.Menu.Values;
+
+    using Color = System.Drawing.Color;
+
+    internal static class ManaManager
+    {
+        internal static bool SpellFarm;
+        internal static bool SpellHarass;
+
+        internal static void AddSpellFarm(Menu mainMenu)
+        {
+            mainMenu.AddSeparator();
+            mainMenu.AddGroupLabel("Farm Settings");
+            mainMenu.Add("SpellFarm", new CheckBox("Use Spell Farm(Mouse Score Control)"));
+            var key = mainMenu.Add("SpellHarass",
+                new KeyBind("Use Spell Harass Enemy(in Farm Mode)", true, KeyBind.BindTypes.PressToggle, 'H'));
+
+            SpellFarm = mainMenu["SpellFarm"].Cast<CheckBox>().CurrentValue;
+            SpellHarass = mainMenu["SpellHarass"].Cast<KeyBind>().CurrentValue;
+
+            key.OnValueChange += delegate (ValueBase<bool> sender, ValueBase<bool>.ValueChangeArgs Args)
+            {
+                SpellHarass = Args.NewValue;
+            };
+
+            Game.OnWndProc += delegate (WndEventArgs Args)
+            {
+                if (Args.Msg == 0x20a)
+                {
+                    mainMenu["SpellFarm"].Cast<CheckBox>().CurrentValue = !SpellFarm;
+                    SpellFarm = mainMenu["SpellFarm"].Cast<CheckBox>().CurrentValue;
+                }
+            };
+        }
+
+        internal static void AddDrawFarm(Menu mainMenu)
+        {
+            mainMenu.AddSeparator();
+            mainMenu.AddLabel("Draw Farm Logic");
+            mainMenu.Add("DrawFarm", new CheckBox("Draw Spell Farm Status"));
+            mainMenu.Add("DrawHarass", new CheckBox("Draw Spell Harass Status"));
+
+            Drawing.OnDraw += delegate 
+            {
+                if (!Player.Instance.IsDead && !MenuGUI.IsChatOpen)
+                {
+                    if (mainMenu["DrawFarm"].Cast<CheckBox>().CurrentValue)
+                    {
+                        var MePos = Drawing.WorldToScreen(Player.Instance.Position);
+
+                        Drawing.DrawText(MePos[0] - 57, MePos[1] + 48, Color.FromArgb(242, 120, 34),
+                            "Spell Farm:" + (SpellFarm ? "On" : "Off"));
+                    }
+
+                    if (mainMenu["DrawHarass"].Cast<CheckBox>().CurrentValue)
+                    {
+                        var MePos = Drawing.WorldToScreen(Player.Instance.Position);
+
+                        Drawing.DrawText(MePos[0] - 57, MePos[1] + 68, Color.FromArgb(242, 120, 34),
+                            "Spell Haras:" + (SpellHarass ? "On" : "Off"));
+                    }
+                }
+            };
+        }
+    }
+}
